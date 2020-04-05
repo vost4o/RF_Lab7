eb54a68632d5809f4b8dcd739389419dbf21c075
vost4o <vasyl.ostafi@gmail.com>
2020-04-05 19:15:49 +0300

    Calculate the free term and make sure to mark the pattern, which class we want to find out ( marked with -1 at the class position in the data set )

--- src/ro/usv/rf/Classifier.java
+++ src/ro/usv/rf/Classifier.java
@@ -25,34 +25,58 @@ class Classifier {
 		classesMap = new HashMap<Integer, ArrayList<Integer>>();
 	}
 
+	/**
+	 * Remember all the classes indices to find them more easily at the calculations
+	 */
 	private void createClassesMap() {
 		for (int x = 0; x < dataSet.length; ++x) {
 			int classType = (int) dataSet[x][dataSet[x].length - 1];
-			if (!classesMap.containsKey(classType)) {
-				classesMap.put((int) classType, new ArrayList<Integer>());
+			// Ignore the -1 class ( the pattern class we need to find out )
+			if (classType != -1) {
+				if (!classesMap.containsKey(classType)) {
+					classesMap.put((int) classType, new ArrayList<Integer>());
+				}
+				classesMap.get(classType).add(x);
 			}
-			classesMap.get(classType).add(x);
 		}
 
 		createWMatrix();
 	}
 
+	/**
+	 * Creates the W-Matrix
+	 */
 	private void createWMatrix() {
 		int classCount = classesMap.size();
 		int parameterCount = dataSet[0].length;
 
 		wMatrix = new double[classCount][parameterCount];
 		for (int i = 0; i < parameterCount; ++i) {
-			for (int classType : classesMap.keySet()) {
-				List<Integer> indexes = classesMap.get(classType);
-				for (int j = 0; j < indexes.size(); ++j) {
-					wMatrix[classType - 1][i] += dataSet[indexes.get(j)][i];
+			if (i == parameterCount - 1) {
+				// Calculate the free term
+				for (int x = 0; x < wMatrix.length; x++) {
+					for (int y = 0; y < wMatrix[0].length - 1; y++) {
+						wMatrix[x][i] += (-1 / 2.0) * Math.pow(wMatrix[x][y], 2);
+					}
 				}
-				wMatrix[classType - 1][i] /= indexes.size();
+			} else {
+				// Calculate the w1..n term
+				for (int classType : classesMap.keySet()) {
+					// Go through all classes
+					List<Integer> indexes = classesMap.get(classType);
+					for (int j = 0; j < indexes.size(); ++j) {
+						wMatrix[classType - 1][i] += dataSet[indexes.get(j)][i];
+					}
+					// Don't forget to divide to number of pattern's of the coresponding class
+					wMatrix[classType - 1][i] /= indexes.size();
+				}
 			}
 		}
 	}
 
+	/**
+	 * A method to check the W-Matrix content (for debug purposes)
+	 */
 	@Override
 	public String toString() {
 		int wMatrixRows = wMatrix.length;
